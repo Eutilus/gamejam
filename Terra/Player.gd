991b2160cd55e0fbcf61efd2247637462399bbdb
Eutilus <kreutzermaik123@web.de>
2019-11-09 01:54:35 +0100

    movement system with fireball

---
+++ Terra/Player.gd
@@ -0,0 +1,41 @@
+extends KinematicBody2D
+
+const MOVE_SPEED = 300
+
+const FIREBALL = preload("res://fireball.tscn")
+
+onready var raycast = $RayCast2D
+
+func _ready():
+	yield(get_tree(), "idle_frame")
+	get_tree().call_group("zombies", "set_player", self)
+
+func _physics_process(delta):
+	var move_vec = Vector2()
+	var direction = Vector2()
+	var velocity = Vector2()
+	if Input.is_action_pressed("move_up"):
+		move_vec.y -= 1
+	if Input.is_action_pressed("move_down"):
+		move_vec.y += 1
+	if Input.is_action_pressed("move_left"):
+		move_vec.x -= 1
+	if Input.is_action_pressed("move_right"):
+		move_vec.x += 1
+	move_vec = move_vec.normalized()
+	move_and_collide(move_vec * MOVE_SPEED * delta)
+
+	var look_vec = get_global_mouse_position() - global_position
+	global_rotation = atan2(look_vec.y, look_vec.x)
+
+	if Input.is_action_just_pressed("shoot"):
+		var fireball = FIREBALL.instance()
+		get_parent().add_child(fireball)
+		fireball.position = $Position2D.global_position
+		fireball.rotation = rotation
+		fireball.velocity = look_vec.normalized()
+		
+
+
+func kill():
+	get_tree().reload_current_scene()
